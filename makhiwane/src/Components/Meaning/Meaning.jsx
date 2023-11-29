aa1b0c2362e3736bb0f686de740be138a37127ce
Yamkela <ncby9zfs7@vossie.net>
2023-11-29 22:16:10 +0200

    SEO

--- makhiwane/src/Components/Meaning/Meaning.jsx
+++ makhiwane/src/Components/Meaning/Meaning.jsx
@@ -15,7 +15,7 @@ const Meaning = () => {
             <article className="mission-article-writing-and-button">
                 <section className="">
                     <h3>MAKHIWANE : MEANING AND ORIGIN</h3>
-                    <p>Makhiwane may be described as a Xhosa term that refers to the art of humanity or ubuntu, namely building each other up. It is derived from the Xhosa word "ukwakha" meaning building something up, and may be described as a call for humans to start pouring into the journeys of those around them. 
+                    <p>Makhiwane may be described as a Xhosa term that refers to the art of humanity or ubuntu, namely uplifting one another. It is derived from the Xhosa word "ukwakha" meaning building something up, and may be described as a call for humans to start pouring into the journeys of those around them. 
                         Its meaning is closely aligned to the ethos of Makhiwane Business Development, which include building up small businesses as well as organizations, and ultimately communities through empowering training seminars, and customized solutions.  
                      </p>
                 </section>
